955c1c28806b79a75e4e19cdd6295834016d4720
Joshua De Marco <joshua.dm@hispeed.ch>
2020-07-30 23:00:12 +0200

    added RGB features

--- src/accessory.ts
+++ src/accessory.ts
@@ -8,49 +8,104 @@ import {
   CharacteristicValue,
   HAP,
   Logging,
-  Service
+  Service,
+  Int8
 } from "homebridge";
+import { callbackify } from "util";
+import { hasUncaughtExceptionCaptureCallback } from "process";
+const colorsys = require('colorsys');
 
 let hap: HAP;
 
-//TEST
-
-
 export = (api: API) => {
   hap = api.hap;
-  api.registerAccessory("ExampleSwitch", ExampleSwitch);
+  api.registerAccessory("Switch", Switch);
 };
 
-class ExampleSwitch implements AccessoryPlugin {
+class Switch implements AccessoryPlugin {
 
   private readonly log: Logging;
   private readonly name: string;
-  private switchOn = false;
-
-  private readonly switchService: Service;
+  private readonly config: AccessoryConfig;
+  private power: number;
+  private brightness: number;
+  private saturation: number;
+  private hue: number;
+  private bulbOn = false;
   private readonly informationService: Service;
+  private readonly lightbulbService: Service;
 
   constructor(log: Logging, config: AccessoryConfig, api: API) {
     this.log = log;
+    this.config = config;
     this.name = config.name;
+    this.power = 0;
+    this.brightness = 100;
+    this.saturation = 0;
+    this.hue = 0;
 
-    this.switchService = new hap.Service.Switch(this.name);
-    this.switchService.getCharacteristic(hap.Characteristic.On)
+
+    this.lightbulbService = new hap.Service.Lightbulb(this.name);
+    this.lightbulbService.getCharacteristic(hap.Characteristic.On)
       .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        log.info("Current state of the switch was returned: " + (this.switchOn? "ON": "OFF"));
-        callback(undefined, this.switchOn);
+        log.info("GET of Bulb: " + (this.bulbOn? "ON":"OFF"));
+        callback(undefined, this.bulbOn);
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        this.switchOn = value as boolean;
-        log.info("Switch state was set to: " + (this.switchOn? "ON": "OFF"));
+        this.bulbOn = value as boolean;
+        log.info("SET of Bulb: " + (this.bulbOn? "ON":"OFF"));
+        callback();
+      });
+    this.lightbulbService.getCharacteristic(hap.Characteristic.Hue)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("GET Hue: " + (this.hue));
+        callback(undefined, this.hue);
+      })
+      .on(CharacteristicEventTypes.SET, (callback: CharacteristicGetCallback) => {
+        this.setColor();
+        log.info("SET Hue: " + (this.hue));
+        callback();
+      });
+    this.lightbulbService.getCharacteristic(hap.Characteristic.Saturation)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("GET Saturation: " + (this.saturation));
+        callback(undefined, this.saturation);
+      })
+      .on(CharacteristicEventTypes.SET, (callback: CharacteristicGetCallback) => {
+        this.setColor();
+        log.info("SET Saturation: " + (this.saturation));
+        callback();
+      });
+    this.lightbulbService.getCharacteristic(hap.Characteristic.Brightness)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("GET Brightness: " + (this.brightness));
+        callback(undefined, this.power);
+      })
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        this.setColor();
+        log.info("SET of Bulb: " + (this.brightness))
         callback();
       });
 
+
     this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
-      .setCharacteristic(hap.Characteristic.Model, "Custom Model");
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Joshua De Marco")
+      .setCharacteristic(hap.Characteristic.Model, "Arduino Lightstrip LED");
 
     log.info("Switch finished initializing!");
+  }
+
+  setColor(): void{
+    const color = colorsys.hsv_to_rgb({
+      h: this.hue,
+      s: this.saturation,
+      v: this.brightness
+    });
+    if(!this.power){
+      color.r = 0;
+      color.g = 0;
+      color.b = 0;
+    }
   }
 
 
@@ -63,7 +118,7 @@ class ExampleSwitch implements AccessoryPlugin {
   getServices(): Service[] {
     return [
       this.informationService,
-      this.switchService,
+      this.lightbulbService,
     ];
   }
 
